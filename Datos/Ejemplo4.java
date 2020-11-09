f0c978cc1b4fea2659c64ed5707edc25b2dba45f
NULL <72146375+nulo-dev@users.noreply.github.com>
2020-11-09 19:15:33 +0100

    Create Ejemplo4.java

---
+++ Datos/Ejemplo4.java
@@ -0,0 +1,4 @@
+Scanner teclat = new Scanner(System.in);
+        System.out.print("Escribe un valor: ");
+        int valor = teclat.nextInt();
+        System.out.println("El valor tecleado es: "+ valor);
