2227f5fa6dbd0a4ca4e8dd6cd31fd526c20dc241
NULL <72146375+nulo-dev@users.noreply.github.com>
2020-11-09 19:16:09 +0100

    Create Ejemplo5.java

---
+++ Datos/Ejemplo5.java
@@ -0,0 +1,4 @@
+Scanner teclat = new Scanner(System.in);
+        System.out.print("Escribe un valor: ");
+        double valor = teclat.nextDouble();
+        System.out.println("El valor tecleadp es: "+ valor);
